883c098658b4c40cfd6a443628e86a3ae72c0718
Evgeniia Briukhova <eve.briukhova897@gmail.com>
2024-05-17 17:19:27 +0600

    add Lesson4 task3

---
+++ Lesson4/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Считать с консоли строку, сотоящую из
+цифр и латинских букв.
+
+Сформировать новую строку, состоящую
+из букв исходной сторки. */
+
+
+string GetLettersFromString(string s)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true) //проверяем явл ли символ буквенным
+        {
+            letters = letters + e; // привяжет букву к зачению e
+        }
+        return letters;
+    }
+}
+string str = Console.ReadLine();
+string result = GetLettersFromString(str);// вункцию GetLetters поместили в переменную result
+Console.WriteLine(result);
